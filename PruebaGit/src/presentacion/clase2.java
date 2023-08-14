65cb9f7fdfd721643ccf2e7609a73b629d62084f
DanielMuniz <dmunizv@gmail.com>
2023-08-14 14:51:30 -0300

    ultimo

--- PruebaGit/src/presentacion/clase2.java
+++ PruebaGit/src/presentacion/clase2.java
@@ -5,6 +5,7 @@ public class clase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hola de pepe");
+		System.out.print("Hola de pepe  22222");
 
 	}
 
